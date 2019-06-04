375b5e59c25ed6d0c6725e4ec767ade0b87581d2
srinivas <srinivas>
2019-06-04 11:10:47 +0530

    intial commit

---
+++ src/main/java/com/wavelabs/CouchBasePOC/CouchBasePocApplication.java
@@ -0,0 +1,13 @@
+package com.wavelabs.CouchBasePOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouchBasePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouchBasePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wavelabs/CouchBasePOC/CouchBasePocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wavelabs.CouchBasePOC;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CouchBasePocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
